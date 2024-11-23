9f087b084bc9bc4c58594a8cedea139f314e6692
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 03:25:29 +0200

    Add new Article model

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\News;
 
 class NewsController extends Controller
 {
-    //
+    
+    function get_news($id = 0){
+
+    }
 }
