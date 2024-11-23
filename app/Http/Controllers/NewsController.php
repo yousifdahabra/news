e3d5facdd683920cefa93d27d3afbdb92c9ba83d
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 03:39:43 +0200

    Update NewsController by adding new function get_news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,6 +9,16 @@ class NewsController extends Controller
 {
     
     function get_news($id = 0){
-
+        if(is_int($id)){
+            $news =  $id ? News::find($id):News::all();
+            return response()->json([
+                'news'=>$news,
+                'message'=>'',
+            ]);
+        }
+        return response()->json([
+            'news'=>null,
+            'message'=>'check news id',
+        ],400);
     }
 }
