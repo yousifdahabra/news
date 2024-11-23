aaf56eaeb3f47ab6e27575ad9a97730164666488
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 23:56:22 +0200

    Add article api

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    function get_article($news_id,$id = 0,$request){
+        $validated = $request->validate([
+            'news_id' => 'required|integer',
+        ]);
+
+        $user = auth('api')->user();
+ 
+        if(is_numeric($id)){
+            $article =  $id ? Article::find($id)->where('news_id', $news_id) : Article::where('news_id', $news_id)->where('is_approved', '1')->get();
+            return response()->json([
+                'article'=>$article,
+                'message'=>'',
+            ]);
+        }
+        return response()->json([
+            'article'=>null,
+            'message'=>'check article id',
+        ],400);
+    }
+    function add_article(Request $request) {
+
+        $validated = $request->validate([
+            'news_id' => 'required|integer',
+            'user_id' => 'required|integer',
+            'title' => 'required|unique:article|max:255',
+            'content' => 'required',
+            'is_approved' => 'required',
+        ]);
+        $article = Article::create($request->all());
+        return response()->json([
+            "article" => $article,
+            'message'=>'succ',
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -16,7 +17,16 @@ use App\Http\Controllers\UserController;
 |
 */
 Route::post("/", [UserController::class, "authenticate"]);
-Route::post("/get_user", [UserController::class, "get_user"]);
+
+Route::prefix("/news")->group(function() {
+    Route::get("/{id?}", [NewsController::class, "get_news"]);
+    Route::post("/", [NewsController::class, "add_news"]);
+    Route::put("/{id}", [NewsController::class, "update_news"]);
+});
+Route::prefix("/article")->group(function() {
+    Route::get("/{news_id}/{id?}", [ArticleController::class, "get_article"]);
+    Route::post("/", [ArticleController::class, "add_article"]);
+});
 
 Route::prefix("/admin")->group(function() {
     Route::prefix("/news")->group(function() {
