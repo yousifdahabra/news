9e55e93e3d3de53d8c7c670d763695bc15f86c7c
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 03:49:59 +0200

    Update api route by add news group

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,7 +10,7 @@ class NewsController extends Controller
     
     function get_news($id = 0){
         if(is_int($id)){
-            $news =  $id ? News::find($id):News::all();
+            $news =  $id ? News::find($id)->where('is_approved', '1') : News::all()->where('is_approved', '1');
             return response()->json([
                 'news'=>$news,
                 'message'=>'',
