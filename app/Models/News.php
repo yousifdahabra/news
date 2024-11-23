79bd67e21187989e374e39989eb6cd4233fe3905
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 03:16:35 +0200

    Update news model by add fillable

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_approved',
+    ];
 }
