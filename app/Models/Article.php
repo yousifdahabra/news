b428415cc156376c10721e6b226e75ac386bdb65
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-23 03:26:26 +0200

    Update Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_approved',
+        'news_id',
+    ];
+
 }
